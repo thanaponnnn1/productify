c621c5e28c4531887decf3e70dd94933a88ca7cf
thanaponnnn1 <emailthanaponnnn1@xxx.com>
2026-05-10 14:52:58 +0700

    feat: add React and Vite setup with initial assets and styles
    - Added React SVG logo to assets
    - Added Vite SVG logo to assets
    - Created global CSS styles for light and dark themes
    - Set up main entry point for React application
    - Configured Vite for React development

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import Express from "express";
+import cors from "cors";
+import path from "path";
+import { clerkMiddleware } from "@clerk/express";
+import { ENV } from "./config/env";
+
+const app = Express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
+// `credentials: true` allows the frontend to send cookies to the backend so that we can authenticate the user.
+app.use(clerkMiddleware()); // auth obj will be attached to the req
+app.use(Express.json()); // parses JSON request bodies.
+app.use(Express.urlencoded({ extended: true })); // parses form data (like HTML forms).
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+});
